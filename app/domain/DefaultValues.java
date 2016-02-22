de3c50d4fff7e131384708951c21a53c7a0dcda3
keithlei01 <keithlei01@gmail.com>
2016-02-22 11:49:42 +0800

    Cleanup

--- app/domain/DefaultValues.java
+++ app/domain/DefaultValues.java
@@ -1,12 +1,5 @@
 package domain;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import models.UserInfo;
-
-import org.joda.time.DateTime;
-
 public class DefaultValues {
     public static final String DELIMITER_COMMA = ",";
     
@@ -36,17 +29,6 @@ public class DefaultValues {
     
     public static final int MAX_ITEMS_COUNT = 1000;
     
-    public static Map<String, String> PARENT_BIRTH_YEARS = new LinkedHashMap<String, String>();
-    public static Map<String, String> CHILD_BIRTH_YEARS = new LinkedHashMap<String, String>();
-    
-    public static final int PARENT_YEAR_MIN_AGE = 16;
-    public static final int PARENT_YEAR_MAX_AGE = 50;
-    public static final int CHILD_YEAR_MIN_AGE = -1;
-    public static final int CHILD_YEAR_MAX_AGE = 14;
-
-    public static final int DUMMY_BIRTH_YEAR = -1;
-    public static final UserInfo.ParentType DUMMY_PARENT_TYPE = UserInfo.ParentType.NA;
-    
     public static final String GOOGLEMAP_PREFIX = "http://maps.google.com.hk/maps?q=";
 
     static {
@@ -54,18 +36,6 @@ public class DefaultValues {
     }
     
     private static void init() {
-        int year = new DateTime().getYear();
         
-        // parent age range
-        for (int i = PARENT_YEAR_MIN_AGE; i <= PARENT_YEAR_MAX_AGE; i++) {
-            PARENT_BIRTH_YEARS.put(String.valueOf(year - i), String.valueOf(year - i));
-        }
-        PARENT_BIRTH_YEARS.put(String.valueOf(year - PARENT_YEAR_MAX_AGE) + "之前", "<" + String.valueOf(year - PARENT_YEAR_MAX_AGE));
-        
-        // child age range
-        for (int i = CHILD_YEAR_MIN_AGE; i <= CHILD_YEAR_MAX_AGE; i++) {
-            CHILD_BIRTH_YEARS.put(String.valueOf(year - i), String.valueOf(year - i));
-        }
-        CHILD_BIRTH_YEARS.put(String.valueOf(year - CHILD_YEAR_MAX_AGE) + "之前", "<" + String.valueOf(year - CHILD_YEAR_MAX_AGE));
     }
 }
