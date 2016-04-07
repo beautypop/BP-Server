9cbc63d6a3d292cd16c428cf6125217d19cb7af7
keithlei01 <keithlei01@gmail.com>
2016-04-07 11:23:35 +0800

    Update cover banner

--- sql/20160406_schema_upgrade_push_notification_token.sql
+++ sql/20160406_schema_upgrade_push_notification_token.sql
@@ -1,2 +1,6 @@
 alter table GcmToken change column regId token VARCHAR(255);
 alter table GcmToken rename PushNotificationToken;
+
+-- after server starts up
+update PushNotificationToken set appVersion='';
+update PushNotificationToken set deviceType='ANDROID';
