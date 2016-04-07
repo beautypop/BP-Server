d2164bac9462c8b402938f74aff851920f76c4cd
keithlei01 <keithlei01@gmail.com>
2016-04-07 10:45:50 +0800

    minor

--- sql/20160406_schema_upgrade_push_notification_token.sql
+++ sql/20160406_schema_upgrade_push_notification_token.sql
@@ -1,2 +1,2 @@
-alter table GcmToken change column regId token VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+alter table GcmToken change column regId token VARCHAR(255);
 alter table GcmToken rename PushNotificationToken;
