c9f0fcb18c95256c585fa564868dbccd830d410b
keithlei01 <keithlei01@gmail.com>
2016-04-18 11:15:58 +0800

    Fix user.name to user.displayName

--- app/events/listener/FollowEventListener.java
+++ app/events/listener/FollowEventListener.java
@@ -60,7 +60,7 @@ public class FollowEventListener extends EventListener {
                                 // Push notification
                                 PushNotificationSender.sendNewFollowNotification(
                                         user.id, 
-                                        localUser.name);
+                                        localUser.displayName);
                             }
                         });
     		}
